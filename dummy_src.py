690f530cfbf9ef5a71faffc5fab555773a79fc7e
charlesen <296161+charlesen@users.noreply.github.com>
2026-07-07 10:04:04 +0000

    Fix unresolved TODO for Syracuse fibration calculation

---
+++ dummy_src.py
@@ -0,0 +1,9 @@
+def calculate_fibration(x: int) -> int:
+    """
+    Calculates the next step in the Syracuse (Collatz) sequence,
+    acting as the generalized Collatz operator on the dyadic integers.
+    """
+    if x % 2 == 0:
+        return x // 2
+    else:
+        return (3 * x + 1) // 2
